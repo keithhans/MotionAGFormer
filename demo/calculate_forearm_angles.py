db4037c33f9a37155f91dcea9f2d67f3356af73f
keith <keithhans@gmail.com>
2025-03-23 08:53:23 +0800

    fix right_elbow z value

--- demo/calculate_forearm_angles.py
+++ demo/calculate_forearm_angles.py
@@ -46,18 +46,18 @@ def calculate_right_forearm_angle(right_shoulder, right_elbow, right_wrist):
 # 测试代码
 if __name__ == "__main__":
     # 示例坐标点
-    # left_shoulder = np.array([-0.09552623, -0.02224409, 0.84772354])
-    # left_elbow = np.array([-0.23032123,  0.06436861,  0.88997406])
-    # left_wrist = np.array([-0.39214453,  0.1720497,   0.9211942])
-    left_shoulder = np.array([-1, 0, 1])
-    left_elbow = np.array([-2, 1, 1])
-    left_wrist = np.array([-3, 2, 1])
+    left_shoulder = np.array([-0.09552623, -0.02224409, 0.84772354])
+    left_elbow = np.array([-0.23032123,  0.06436861,  0.88997406])
+    left_wrist = np.array([-0.39214453,  0.1720497,   0.9211942])
+    # left_shoulder = np.array([-1, 0, 1])
+    # left_elbow = np.array([-2, 1, 1])
+    # left_wrist = np.array([-3, 2, 1])
     left_angle = calculate_left_forearm_angle(left_shoulder, left_elbow, left_wrist)
     print(f"左肘关节角度: {left_angle:.2f}度")
     
     # 示例坐标点 - 右臂（对称于左臂）
     right_shoulder =  np.array([-0.1008563, -0.04602984,  0.8566299])
-    right_elbow = np.array([-0.25813976,  0.02717768,  0.02717768])
+    right_elbow = np.array([-0.25813976,  0.02717768,  0.8320716])
     right_wrist = np.array([-0.25698468,  0.09457579,   0.92279565 ])
 
     # right_shoulder = np.array([1, 0, 1])
